f1e2663c33742a07c80ac5889b867ebcdf0e49b3
Jacobgijah <jacobgijjah@gmail.com>
2024-08-26 13:30:19 +0300

    restructure backend modify function

--- hopechain-engine/src/declarations/hopechain-engine-backend/hopechain-engine-backend.did.js
+++ hopechain-engine/src/declarations/hopechain-engine-backend/hopechain-engine-backend.did.js
@@ -1,32 +1,40 @@
 export const idlFactory = ({ IDL }) => {
+  const ProductID = IDL.Nat;
   const Product = IDL.Record({
+    'id' : ProductID,
     'inventory' : IDL.Nat,
     'productName' : IDL.Text,
+    'sellerName' : IDL.Text,
     'shortDescription' : IDL.Text,
     'currency' : IDL.Text,
+    'category' : IDL.Text,
     'price' : IDL.Float64,
     'productImage' : IDL.Vec(IDL.Nat8),
     'longDescription' : IDL.Text,
     'dateAdded' : IDL.Text,
   });
-  const User = IDL.Record({ 'name' : IDL.Text });
+  const UserID = IDL.Nat;
+  const User = IDL.Record({ 'id' : UserID, 'name' : IDL.Text });
   return IDL.Service({
     'addProduct' : IDL.Func(
         [
           IDL.Text,
           IDL.Text,
           IDL.Text,
+          IDL.Text,
           IDL.Float64,
+          IDL.Text,
           IDL.Text,
           IDL.Vec(IDL.Nat8),
           IDL.Nat,
           IDL.Text,
         ],
-        [],
+        [ProductID],
         [],
       ),
     'deposit' : IDL.Func([IDL.Float64, IDL.Text], [IDL.Float64], []),
     'getProducts' : IDL.Func([], [IDL.Vec(Product)], ['query']),
+    'getProductsBySeller' : IDL.Func([IDL.Text], [IDL.Vec(Product)], ['query']),
     'getTotalCharityAmount' : IDL.Func([], [IDL.Float64], ['query']),
     'getTotalPrice' : IDL.Func([], [IDL.Float64], ['query']),
     'getUser' : IDL.Func([IDL.Text], [IDL.Opt(User)], ['query']),
